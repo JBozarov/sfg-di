6ae4f95189b12260bb892b5499c46dec1b227c64
John Bozarov <codinginusa@gmail.com>
2020-08-23 14:46:30 -0400

    First commit

---
+++ src/main/java/guru/springframework/sfgdi/services/GreenServiceImpl.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgdi.services;
+
+public class GreenServiceImpl implements GreetingService {
+
+	@Override
+	public String sayGreeting() {
+		
+		return "Hello World";
+	}
+
+}
