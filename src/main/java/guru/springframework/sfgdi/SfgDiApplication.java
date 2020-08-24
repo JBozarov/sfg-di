a1ee42adc4b2cdb33a6623efe9f099f21da68df0
John Bozarov <codinginusa@gmail.com>
2020-08-24 12:53:39 -0400

    @Controller and @Autowired implemented in controllers

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 
 
 @SpringBootApplication
@@ -14,9 +16,23 @@ public class SfgDiApplication {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 		
 		MyController myController = (MyController) ctx.getBean("myController"); 
+		
 		String greeting = myController.sayHello(); 
+		
 		System.out.println(greeting);
 		
+		System.out.println("-------------Property");
+		
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController"); 
+		
+		System.out.println(propertyInjectedController.getGreeting());
+		
+		System.out.println("-----------Setters");
+		
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		
+		System.out.println(setterInjectedController.getGreeting());
+
 	}
 
 }
